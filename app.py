5cc73dbce32610b05bde14e583c96e29dde328cd
Elcio Ferreira <elcio@visie.com.br>
2025-12-05 14:22:31 -0300

    Mais um teste de log

--- app.py
+++ app.py
@@ -1,6 +1,9 @@
 import json
 import mysql.connector
+import logging
 from flask import Flask
+
+logger=logging.getLogger('seguro')
 
 with open('config.json') as config_file:
     config = json.load(config_file)
@@ -14,5 +17,5 @@ def index():
     cursor = mydb.cursor()
     cursor.execute("SELECT * FROM pessoa")
     results = [{'id': d[0], 'nome': d[1]} for d in cursor.fetchall()]
-    print('Acesso!')
+    logger.info('Acesso!')
     return {"result": results, "status": "success"}
