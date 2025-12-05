4741cb71b1249a20742392c5cd967f19393435f9
Elcio Ferreira <elcio@visie.com.br>
2025-12-05 13:08:44 -0300

    Importação inicial

---
+++ app.py
@@ -0,0 +1,17 @@
+import json
+import mysql.connector
+from flask import Flask
+
+with open('config.json') as config_file:
+    config = json.load(config_file)
+
+mydb = mysql.connector.connect(**config['db'])
+
+app = Flask(__name__)
+
+@app.get('/')
+def index():
+    cursor = mydb.cursor()
+    cursor.execute("SELECT * FROM pessoa")
+    results = [{'id': d[0], 'nome': d[1]} for d in cursor.fetchall()]
+    return {"pessoas": results}
